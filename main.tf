8447b19a64a432168233f301cff51b0787788d3e
terraformboot camp <terraformboot@gmail.com>
2023-10-20 04:56:14 +0000

    #10 install terraform provider, create s3 bucket

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.22.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,8 +20,14 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower            = true
+  upper            = false
+  length           = 32
   special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "randmom_bucket_name" {
