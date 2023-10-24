9966c1cca4c6cbe4d9faa88564b56aba7da85f22
terraformboot camp <terraformboot@gmail.com>
2023-10-24 04:39:46 +0000

    #13 migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "ExamPro"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
@@ -6,30 +20,32 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "5.22.0"
+      version = "5.16.2"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
 }
-
 provider "random" {
   # Configuration options
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  lower            = true
-  upper            = false
-  length           = 32
-  special          = false
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 }
 
-output "randmom_bucket_name" {
+output "random_bucket_name" {
   value = random_string.bucket_name.result
 }
