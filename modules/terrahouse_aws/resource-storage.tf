0754b4f263af43990becbe696a0c9e7d1bc65d6e
terraformboot camp <terraformboot@gmail.com>
2023-11-16 03:45:47 +0000

    #32 only upload index.html if a content version has change

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -45,7 +45,12 @@ resource "aws_s3_object" "error_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.error_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
+
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -68,4 +73,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         }
       },
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
