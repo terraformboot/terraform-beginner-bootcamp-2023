9e45a22b8bb19abab481e2ad1a9bfda4bd7576a5
terraformboot <146905343+terraformboot@users.noreply.github.com>
2023-11-21 22:02:10 -0600

    #34 invalidate cloudfront stack when content version changes (#35)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "s3_website_endpoint" {
   description = "S3 Static Website hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The Cloudfron Distribution Domain"
+  value = module.terrahouse_aws.cloudfront_url
+  
+}
