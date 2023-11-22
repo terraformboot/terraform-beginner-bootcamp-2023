0d678a9b9585f02431175958cf6a9cabf1fddff9
terraformboot camp <terraformboot@gmail.com>
2023-11-22 04:01:09 +0000

    #34 invalidate cloudfront stack when content version changes

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -68,3 +68,17 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 data "aws_caller_identity" "current" {
 
 }
+
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  provisioner "local-exec" {
+  # https://developer.hashicorp.com/terraform/language/expressions/strings#heredoc-strings
+    command = <<COMMAND
+aws cloudfront create-invalidation \
+--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+--paths '/*'
+    COMMAND
+
+  }
+}
